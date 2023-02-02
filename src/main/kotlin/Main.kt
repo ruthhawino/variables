02a12adb84bf38c461db85a1e2c3b49cb723f06c
awinoruthh <awinoruthh@gmail.com>
2023-02-02 16:30:22 +0300

    muted variables

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+
+    var fullname = "awino"
+    var age = 19
+    var phonenumber = "0798970706"
+    val kenyan: Boolean = true
+    val notkenyan: Boolean = false
+    println(age)
+}
